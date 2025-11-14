072331d379533cc01d21d95cdd3b2b9d6d4f838f
renz0826 <brenzbedonia@gmail.com>
2025-11-15 00:38:47 +0800

    refactor: specified nav items

--- components/nav.php
+++ components/nav.php
@@ -5,21 +5,15 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
     <nav class="site-nav" aria-label="Main navigation">
   <ul>
     <li><a href="home.php">Home</a></li>
-    <li><a href="create_capsule.php">Create</a></li>
-    <li><a href="view_capsule.php">Browse</a></li>
-    <li><a href="edit_capsule.php">Edit</a></li>
+    <li><a href="create_moment.php">Preserve Moment</a></li>
+    <li><a href="view_moment.php">View Moments</a></li>
+    <li><a href="edit_moment.php">Edit Moment</a></li>
+    <li><a href="my_moments.php">My Moments</a></li>
   </ul>
 </nav>
-
-
 </body>
 </html>
