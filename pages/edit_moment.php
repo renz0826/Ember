ade1069bca2c522c173fc99c28f567a66206615f
renz0826 <brenzbedonia@gmail.com>
2025-11-16 22:48:48 +0800

    feat(pages): add header component on pages

--- pages/edit_moment.php
+++ pages/edit_moment.php
@@ -3,21 +3,29 @@
 // includes the head
 require_once __DIR__ . '/../includes/head.php';
 
-// includes navbar component
-require_once __DIR__ . '/../components/nav.php';
-
-// View Capsule
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Moment - Ember</title>
 </head>
+
 <body>
-    
+    <main>
+        <div class="left">
+            <?php require_once __DIR__ . '/../components/nav.php';?>
+
+        </div>
+        <div class="right">
+            <?php require_once __DIR__ . '/../components/header.php'; ?>
+        </div>
+
+    </main>
 </body>
+
 </html>
