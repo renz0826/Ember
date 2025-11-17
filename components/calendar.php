6cc4164e9b0c16eb18ed6c859392f471c03c11cc
Frandolph <frandolphjoseph@gmail.com>
2025-11-17 19:10:55 +0800

    Google Calendar API
    Added Google calendar API initialization

--- components/calendar.php
+++ components/calendar.php
@@ -33,3 +33,38 @@ function renderCalendar($initialDate = '')
 <link rel="stylesheet" href="/Ember/assets/css/calendar.css">
 HTML;
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="/Ember/assets/css/calendar.css">
+    <script src="/Ember/assets/js/calendar.js" defer></script>
+    <script src="https://apis.google.com/js/api.js"></script>
+</head>
+
+<body onload="gapiInit()">
+    <div id="calendar">
+        <h2 id="setDate"></h2>
+        <div class="calendar_header">
+
+            <h4 id="month_year"></h4>
+            <div class="calendar_navigation">
+                <button id="prev"> <img src="/Ember/assets/icons/icon-arrow.svg" alt="icon-arrow" /> </button>
+                <button id="next"> <img style="rotate: 180deg; " src="/Ember/assets/icons/icon-arrow.svg"
+                        alt="icon-arrow" /> </button>
+            </div>
+        </div>
+        <div class="main_calendar">
+            <div class="calendar_week"></div>
+            <div class="calendar_grid"></div>
+        </div>
+
+
+    </div>
+    </div>
+</body>
+
+</html>
