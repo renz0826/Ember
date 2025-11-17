a9b2df37331a8950fb5852d08185701e17890348
renz0826 <brenzbedonia@gmail.com>
2025-11-17 18:13:49 +0800

    fix(style): decrease font size on larger screen witdth

--- components/nav.php
+++ components/nav.php
@@ -29,30 +29,30 @@ $currentTitle = $map[$current] ?? null;
 
 <body>
   <nav class="site-nav" aria-label="Main navigation">
-    <img class="logo" width="106px" src="/Ember/assets/images/logo-typo.png" alt="logo-typo">
+    <img class="logo" width="92px" src="/Ember/assets/images/logo-typo.png" alt="logo-typo">
     <ul>
       <li><a href="home.php" id="homeBtn"
           class="<?=  $current === 'home.php' ? 'active' : ' ' ?>">
-          <img src="/Ember/assets/icons/icon-home.svg" alt="icon-home">
+          <img width="32px" height="32px" src="/Ember/assets/icons/icon-home.svg" alt="icon-home">
           Home
         </a>
       </li>
       <li><a href="preserve_moment.php" id="preserveBtn"
           class="<?=  $current === 'preserve_moment.php' ? 'active' : ' ' ?>">
-          <img src="/Ember/assets/icons/icon-preserve.svg" alt="icon-preserve-moment">
+          <img width="32px" height="32px" src="/Ember/assets/icons/icon-preserve.svg" alt="icon-preserve-moment">
           Preserve a Moment
         </a>
       </li>
       <li><a href="my_moments.php" id="momentsBtn"
           class="<?=  $current === 'my_moments.php' ? 'active' : ' ' ?>">
-          <img src="/Ember/assets/icons/icon-moments.svg" alt="icon-moments">
+          <img width="32px" height="32px" src="/Ember/assets/icons/icon-moments.svg" alt="icon-moments">
           My Moments
         </a>
       </li>
       <li><a href="view_moment.php">View Moments</a></li>
       <li><a href="edit_moment.php">Edit Moment</a></li>
       <li class="logout"><a href="/Ember/index.php">
-          <img src="/Ember/assets/icons/icon-logout.svg" alt="icon-logout">
+          <img width="32px" height="32px" src="/Ember/assets/icons/icon-logout.svg" alt="icon-logout">
           Logout
         </a>
       </li>
