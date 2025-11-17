27c45627cfc3ceb2c9f836a1fc2d4d166be1dd4f
renz0826 <brenzbedonia@gmail.com>
2025-11-17 15:18:08 +0800

    feat(pages): integrate calendar component

--- pages/my_moments.php
+++ pages/my_moments.php
@@ -22,7 +22,18 @@ require_once __DIR__ . '/../includes/head.php';
 
         </div>
         <div class="right">
-            <?php require_once __DIR__ . '/../components/header.php'; ?>
+            <div class="top">
+                <?php require_once __DIR__ . '/../components/header.php'; ?>
+            </div>
+            <div class="bottom">
+                <div class="bottom_left">
+
+                </div>
+                <div class="bottom_right">
+                    <?php require_once __DIR__ . '/../components/calendar.php'; ?>
+                </div>
+
+            </div>
         </div>
 
     </main>
