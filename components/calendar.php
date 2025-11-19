2d541c610f32e9ccbb6615f331a86f1e44e65dd5
Renz Bedonia <brenzbedonia@gmail.com>
2025-11-19 21:52:19 +0800

    refactor(calendar): rendered the calendar using a function

--- components/calendar.php
+++ components/calendar.php
@@ -1,30 +1,29 @@
 <?php
-
 require_once __DIR__ . '/../includes/head.php';
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <link rel="stylesheet" href="/Ember/assets/css/calendar.css">
-    <script src="/Ember/assets/js/calendar.js" defer></script>
-</head>
-    <div id="calendar">
-        <h2 id="setDate"></h2>
-        <div class="calendar_header">
-
-            <h4 id="month_year"></h4>
-            <div class="calendar_navigation">
-                <button id="prev"> <img src="/Ember/assets/icons/icon-arrow.svg" alt="icon-arrow" /> </button>
-                <button id="next"> <img style="rotate: 180deg; " src="/Ember/assets/icons/icon-arrow.svg"
-                        alt="icon-arrow" /> </button>
-            </div>
-        </div>
-        <div class="main_calendar">
-            <div class="calendar_week"></div>
-            <div class="calendar_grid"></div>
+function renderCalendar()
+{
+    echo <<<HTML
+<div id="calendar">
+    <h2 id="setDate"></h2>
+    <div class="calendar_header">
+        <h4 id="month_year"></h4>
+        <div class="calendar_navigation">
+            <button id="prev">
+                <img src="/Ember/assets/icons/icon-arrow.svg" alt="icon-arrow" />
+            </button>
+            <button id="next">
+                <img style="rotate: 180deg;" src="/Ember/assets/icons/icon-arrow.svg" alt="icon-arrow" />
+            </button>
         </div>
     </div>
-</html>
+    <div class="main_calendar">
+        <div class="calendar_week"></div>
+        <div class="calendar_grid"></div>
+    </div>
+</div>
+
+<script src="/Ember/assets/js/calendar.js" defer></script>
+<link rel="stylesheet" href="/Ember/assets/css/calendar.css">
+HTML;
+}
