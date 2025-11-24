4c50e549a40c4a29e1ecd29151b8f4b10ef39063
renz0826 <brenzbedonia@gmail.com>
2025-11-24 20:30:11 +0800

    feat(db): create a database connection

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,3 +1,17 @@
-<?php 
+<?php
 
-?>
+// Database connection parameters
+$host = 'localhost';
+$db   = 'ember_db';
+$user = 'root';
+$pass = '';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn -> connect_error) {
+    error_log("Database connection failed: " . $conn->connect_error);
+} else {
+    echo "<script> console.log(\" Successfully Connected to $db\") </script>";
+
+    $conn->set_charset("utf8mb4");
+}
